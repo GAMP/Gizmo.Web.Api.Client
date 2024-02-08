2a898ffdba72580ae9f73d9f9ce4297b825e4dde
Oleg Savelos <olegsavelos@gmail.com>
2024-02-08 10:38:46 +0200

    updates.

--- Gizmo.Web.Api.Client/Clients/ReportModulesWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/ReportModulesWebApiClient.cs
@@ -12,6 +12,9 @@ using Microsoft.IO;
 
 namespace Gizmo.Web.Api.Clients
 {
+    /// <summary>
+    /// Report modules web api client.
+    /// </summary>
     [WebApiRoute("api/v2/reportmodules")]
     public sealed class ReportModulesWebApiClient : WebApiClientBase
     {
@@ -39,19 +42,27 @@ namespace Gizmo.Web.Api.Clients
             var memoryStream = StreamManager.GetStream();
 
             //call the web api and copy the response stream
-            await PostContentCopyAsync(parameters,(object?)reportPackModel, memoryStream, cancellationToken).ConfigureAwait(false);
+            await PostContentCopyAsync(parameters, (object?)reportPackModel, memoryStream, cancellationToken).ConfigureAwait(false);
 
             //seek to beginning
             memoryStream.Seek(0, SeekOrigin.Begin);
             return memoryStream;
         }
 
-        public async Task<IReportModuleResultModel> GetAsync(Type resultModelType, Guid guid, IDictionary<string, string> reportFilter, CancellationToken cancellationToken = default)
+        /// <summary>
+        /// Generates report.
+        /// </summary>
+        /// <param name="guid">Report module id.</param>
+        /// <param name="resultModelType">Result model type.</param>
+        /// <param name="filters">Report filters.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Report result model.</returns>
+        public async Task<IReportModuleResultModel> GetAsync(Guid guid, Type resultModelType, IDictionary<string, string> filters, CancellationToken cancellationToken = default)
         {
             var genericMethod = GetMethodInfo.MakeGenericMethod(resultModelType);
 
             //TODO : reflection might be faster than dynamic
-            dynamic task = (Task)genericMethod.Invoke(this, new object[] { guid, reportFilter, cancellationToken })!;
+            dynamic task = (Task)genericMethod.Invoke(this, new object[] { guid, filters, cancellationToken })!;
 
             //await for task to complete
             await task;
@@ -59,10 +70,18 @@ namespace Gizmo.Web.Api.Clients
             return (IReportModuleResultModel)task.GetAwaiter().GetResult();
         }
 
-        public Task<TReportModuleResultModel> GetAsync<TReportModuleResultModel>(Guid guid, IDictionary<string, string> reportFilter, CancellationToken cancellationToken = default)
+        /// <summary>
+        /// Generates report.
+        /// </summary>
+        ///<typeparam name="TReportModuleResultModel">Result model type.</typeparam>
+        /// <param name="guid">Report module id.</param>
+        /// <param name="filters">Report filters.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Report result model.</returns>
+        public Task<TReportModuleResultModel> GetAsync<TReportModuleResultModel>(Guid guid, IDictionary<string, string> filters, CancellationToken cancellationToken = default)
             where TReportModuleResultModel : IReportModuleResultModel
         {
-            var parameters = new UriParameters([guid.ToString()], reportFilter);
+            var parameters = new UriParameters([guid.ToString()], filters);
             return GetAsync<TReportModuleResultModel>(parameters, cancellationToken);
         }
     }
