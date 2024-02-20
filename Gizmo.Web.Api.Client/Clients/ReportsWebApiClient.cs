de24f028c2a36224178bdd0682a9e79f9049ca01
klapsis-netprojects <klapsis@netprojects.gr>
2024-02-20 12:27:40 +0200

    Updates.

--- Gizmo.Web.Api.Client/Clients/ReportsWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/ReportsWebApiClient.cs
@@ -25,10 +25,10 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<FinancialReportModel>(parameters, cancellationToken);
         }
 
-        public Task<HostsReportModel> HostsAsync(HostsReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<HostUsageReportModel> HostUsageAsync(HostUsageReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["hosts"], parametersModel);
-            return GetAsync<HostsReportModel>(parameters, cancellationToken);
+            return GetAsync<HostUsageReportModel>(parameters, cancellationToken);
         }
 
         public Task<UsersReportModel> UsersAsync(UsersReportParametersModel parametersModel, CancellationToken cancellationToken = default)
@@ -61,28 +61,28 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<StockReportModel>(parameters, cancellationToken);
         }
 
-        public Task<TransactionsReportModel> TransactionsAsync(TransactionReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<TransactionsReportModel> TransactionsLogAsync(TransactionsLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["transactions"], parametersModel);
             return GetAsync<TransactionsReportModel>(parameters, cancellationToken);
         }
 
-        public Task<ShiftsReportModel> ShiftsAsync(ShiftsReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<ShiftsLogReportModel> ShiftsLogAsync(ShiftsLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["shifts"], parametersModel);
-            return GetAsync<ShiftsReportModel>(parameters, cancellationToken);
+            return GetAsync<ShiftsLogReportModel>(parameters, cancellationToken);
         }
 
-        public Task<AssetsReportModel> AssetsAsync(AssetsReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<AssetsLogReportModel> AssetsLogAsync(AssetsLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["assets"], parametersModel);
-            return GetAsync<AssetsReportModel>(parameters, cancellationToken);
+            return GetAsync<AssetsLogReportModel>(parameters, cancellationToken);
         }
 
-        public Task<InvoicesReportModel> InvoicesAsync(InvoicesReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<InvoicesLogReportModel> InvoicesLogAsync(InvoicesLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["invoices"], parametersModel);
-            return GetAsync<InvoicesReportModel>(parameters, cancellationToken);
+            return GetAsync<InvoicesLogReportModel>(parameters, cancellationToken);
         }
 
         public Task<ZLogReportModel> ZLogAsync(ZLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
@@ -97,10 +97,10 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<ApplicationsReportModel>(parameters, cancellationToken);
         }
 
-        public Task<SessionsReportModel> SessionsAsync(SessionsReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<SessionsLogReportModel> SessionsLogAsync(SessionsLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["sessions"], parametersModel);
-            return GetAsync<SessionsReportModel>(parameters, cancellationToken);
+            return GetAsync<SessionsLogReportModel>(parameters, cancellationToken);
         }
 
         public Task<LicensesReportModel> LicensesAsync(LicensesReportParametersModel parametersModel, CancellationToken cancellationToken = default)
@@ -109,10 +109,10 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<LicensesReportModel>(parameters, cancellationToken);
         }
 
-        public Task<OrdersReportModel> OrdersAsync(OrdersReportParametersModel parametersModel, CancellationToken cancellationToken = default)
+        public Task<OrdersLogReportModel> OrdersLogAsync(OrdersLogReportParametersModel parametersModel, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["orders"], parametersModel);
-            return GetAsync<OrdersReportModel>(parameters, cancellationToken);
+            return GetAsync<OrdersLogReportModel>(parameters, cancellationToken);
         }
     }
 }
