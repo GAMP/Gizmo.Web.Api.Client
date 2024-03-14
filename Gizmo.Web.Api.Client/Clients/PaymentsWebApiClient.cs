3b52998230a55d9e38dfb65d618b177f5424e50f
Oleg Savelos <olegsavelos@gmail.com>
2024-03-14 12:41:55 +0200

    add payments client.

---
+++ Gizmo.Web.Api.Client/Clients/PaymentsWebApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v2/payments")]
+    public sealed class PaymentsWebApiClient : WebApiClientBase
+    {
+        public PaymentsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<PaymentTransactionModel>> TransactionsAsync(PaymentTransactionFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["transactions"],filter);
+            return GetAsync<PagedList<PaymentTransactionModel>>(parameters, cancellationToken);
+        }
+    }
+}
