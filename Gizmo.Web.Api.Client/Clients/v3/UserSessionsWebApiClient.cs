4ddf355cf0abc8b3c95f310d22fa2d8093a0a1bb
Oleg Savelos <olegsavelos@gmail.com>
2024-05-27 13:22:30 +0300

    updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/UserSessionsWebApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/usersessions")]
+    public sealed class UserSessionsWebApiClient : WebApiClientBase
+    {
+        public UserSessionsWebApiClient(HttpClient httpClient,
+            IOptions<WebApiClientOptions> options, 
+            IPayloadSerializerProvider payloadSerializerProvider) : base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<UserSessionModel>> GetAsync(UserSessionFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<UserSessionModel>>(parameters, cancellationToken);
+        }
+
+        public Task<UserSessionModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<UserSessionModel>(parameters, cancellationToken);
+        }
+    }
+}
