b4e288a7e74a4a717a9f56dc7bf3e4a0a1c7f862
klapsis-netprojects <klapsis@netprojects.gr>
2024-08-28 16:47:30 +0300

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/ApplicationLinksWebApiClient.cs
@@ -0,0 +1,56 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/applicationlinks")]
+    public sealed class ApplicationLinksWebApiClient : WebApiClientBase
+    {
+        #region CONSTRUCTOR
+        public ApplicationLinksWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+        #endregion
+
+        #region FUNCTIONS
+
+        public Task<PagedList<ApplicationLinkModel>> GetAsync(ApplicationLinksFilter filter, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<ApplicationLinkModel>>(parameters, ct);
+        }
+
+        public Task<CreateResult> CreateAsync(ApplicationLinkModelCreate model, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, ct);
+        }
+
+        public Task<UpdateResult> UpdateAsync(ApplicationLinkModelUpdate model, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, ct);
+        }
+
+        public Task<ApplicationLinkModel> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<ApplicationLinkModel>(parameters, ct);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, ct);
+        }
+
+        #endregion
+    }
+}
