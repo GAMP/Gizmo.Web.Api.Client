31562b2ce952b529457b30221d1ae1b00d1a7e61
klapsis-netprojects <klapsis@netprojects.gr>
2024-10-22 17:00:50 +0300

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/SessionsWebApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/sessions")]
+    public sealed class SessionsWebApiClient : WebApiClientBase
+    {
+        public SessionsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<SessionModel>> GetAsync(SessionsFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<SessionModel>>(parameters, cancellationToken);
+        }
+    }
+}
---
+++ Gizmo.Web.Api.Client/Clients/v3/TimeProductsWebApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/timeproducts")]
+    public sealed class TimeProductsWebApiClient : WebApiClientBase
+    {
+        public TimeProductsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<TimeProductModel>> GetAsync(TimeProductsFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<TimeProductModel>>(parameters, cancellationToken);
+        }
+    }
+}
