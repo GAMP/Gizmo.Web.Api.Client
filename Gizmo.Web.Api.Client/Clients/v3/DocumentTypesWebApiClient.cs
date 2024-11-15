888ba0884c347913b67183d2a25d4360fb87fd11
Oleg Savelos <olegsavelos@gmail.com>
2024-11-15 16:05:28 +0200

    document api clients.

---
+++ Gizmo.Web.Api.Client/Clients/v3/DocumentTypesWebApiClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/documenttypes")]
+    public sealed class DocumentTypesWebApiClient : WebApiClientBase
+    {
+        public DocumentTypesWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<DocumentTypeModel>> GetAsync(DocumentTypeFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<DocumentTypeModel>>(parameters, cancellationToken);
+        }
+
+        public Task<DocumentTypeModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<DocumentTypeModel>(parameters, cancellationToken);
+        }
+
+        public Task<CreateResult> CreateAsync(DocumentTypeCreateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateAsync(DocumentTypeUpdateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> UnDeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "undelete"]);
+            return PutAsync<UpdateResult>(parameters, cancellationToken);
+        }
+
+    }
+}
---
+++ Gizmo.Web.Api.Client/Clients/v3/DocumentsWebApiClient.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/documents")]
+    public sealed class DocumentsWebApiClient : WebApiClientBase
+    {
+        public DocumentsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<DocumentModel>> GetAsync(DocumentFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<DocumentModel>>(parameters, cancellationToken);
+        }
+
+        public Task<DocumentModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<DocumentModel>(parameters, cancellationToken);
+        }
+
+        public Task<CreateResult> CreateAsync(DocumentCreateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateAsync(DocumentUpdateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> UnDeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "undelete"]);
+            return PutAsync<UpdateResult>(parameters, cancellationToken);
+        }
+
+        public Task<ExistResult> FileNameExistsAsync(string fileName, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["file", fileName, "exists"]);
+            return GetAsync<ExistResult>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> DescriptionAsync(int id, string? description, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "description"]);
+            return PutAsync<UpdateResult>(parameters, description, cancellationToken);
+        }
+
+        public Task<string?> DescriptionAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "description"]);
+            return GetAsync<string?>(parameters, cancellationToken);
+        }
+    }
+}
