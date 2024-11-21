6ea696e2bc3140a2ab3020f347b8307af0bf5103
Oleg Savelos <olegsavelos@gmail.com>
2024-11-21 16:28:57 +0200

    add system web api client.

---
+++ Gizmo.Web.Api.Client/Clients/v3/SystemWebApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Gizmo.Web.Api.Clients;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/system")]
+    public sealed class SystemWebApiClient : WebApiClientBase
+    {
+        public SystemWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<IEnumerable<TimeZoneInfoModel>> TimeZonesAsync()
+        {
+            var parameters = new UriParameters(["timezones"]);
+            return GetAsync<IEnumerable<TimeZoneInfoModel>>(parameters, default);
+        }
+    }
+}
