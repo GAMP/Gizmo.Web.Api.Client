516ec62b5248749de6a34e29faac1c79fe540fe5
Oleg Savelos <olegsavelos@gmail.com>
2024-11-26 18:02:54 +0200

    add all typed options.

--- Gizmo.Web.Api.Client/Clients/v3/OptionsWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/v3/OptionsWebApiClient.cs
@@ -59,18 +59,6 @@ namespace Gizmo.Web.Api.Clients
             return PutAsync<UpdateResult>(UriParameters.Empty, storeOptionsWrite, cancellationToken);
         }
 
-        public Task<GeneralOptions> GeneralAsync(CancellationToken cancellationToken = default)
-        {
-            var parameters = new UriParameters(["general"]);
-            return GetAsync<GeneralOptions>(parameters, cancellationToken);
-        }
-
-        public Task<StoreOptionsReadPack> GeneralPackAsync(CancellationToken cancellationToken = default)
-        {
-            var parameters = new UriParameters(["general", "pack"]);
-            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
-        }
-
         public Task<CurrencyOptions> CurrencyAsync(CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["currency"]);
@@ -81,6 +69,12 @@ namespace Gizmo.Web.Api.Clients
         {
             var parameters = new UriParameters(["currency", "pack"]);
             return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> CurrencyAsync(CurrencyOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["currency"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
         }
 
         public Task<BusinessOptions> BusinessAsync(CancellationToken cancellationToken = default)
@@ -95,6 +89,30 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
         }
 
+        public Task<UpdateResult> BusinessAsync(BusinessOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["business"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<GeneralOptions> GeneralAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["general"]);
+            return GetAsync<GeneralOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> GeneralPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["general", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> GeneralAsync(GeneralOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["general"]);
+            return PostAsync<UpdateResult>(parameters,options, cancellationToken);
+        }
+
         public Task<ManagerFeaturesOptions> ManagerFeaturesAsync(CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(["managerfeatures"]);
@@ -105,6 +123,102 @@ namespace Gizmo.Web.Api.Clients
         {
             var parameters = new UriParameters(["managerfeatures", "pack"]);
             return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> ManagerFeaturesAsync(ManagerFeaturesOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["managerfeatures"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<SubscriptionOptions> SubscriptionAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["subscription"]);
+            return GetAsync<SubscriptionOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> SubscriptionPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["subscription", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> SubscriptionAsync(SubscriptionOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["subscription"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<RegionalOptions> RegionalAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["regional"]);
+            return GetAsync<RegionalOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> RegionalPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["regional", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> RegionalAsync(RegionalOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["regional"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<FiscalizationOptions> FiscalizationAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["fiscalization"]);
+            return GetAsync<FiscalizationOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> FiscalizationPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["fiscalization", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> FiscalizationAsync(FiscalizationOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["fiscalization"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<TaxOptions> TaxAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["tax"]);
+            return GetAsync<TaxOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> TaxPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["tax", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> TaxAsync(TaxOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["tax"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
+        }
+
+        public Task<ReservationsOptions> ReservationsAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["reservations"]);
+            return GetAsync<ReservationsOptions>(parameters, cancellationToken);
+        }
+
+        public Task<StoreOptionsReadPack> ReservationsPackAsync(CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["reservations", "pack"]);
+            return GetAsync<StoreOptionsReadPack>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> ReservationsAsync(ReservationsOptions options, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["reservations"]);
+            return PostAsync<UpdateResult>(parameters, options, cancellationToken);
         }
     }
 }
