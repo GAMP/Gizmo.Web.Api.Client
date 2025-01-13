dd8cca1bcf96fa86aaaf5c320eb9a3600ecbe320
Oleg Savelos <olegsavelos@gmail.com>
2025-01-13 16:05:28 +0200

    add plugin library client.

---
+++ Gizmo.Web.Api.Client/Clients/v3/PluginLibraryWebApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using Gizmo.Web.Api.Models;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/pluginlibrary")]
+    public sealed class PluginLibraryWebApiClient : WebApiClientBase
+    {
+        public PluginLibraryWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<PluginLibraryModel>> GetAsync(PluginLibraryFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<PluginLibraryModel>>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateAsync(PluginLibraryModelUpdate model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
+        }
+    }
+}
