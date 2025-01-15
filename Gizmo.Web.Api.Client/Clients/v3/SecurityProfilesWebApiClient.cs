57e3a11e6e08d3fa0fd625cdd1bf9a6f1af2e72e
Oleg Savelos <olegsavelos@gmail.com>
2025-01-15 18:43:47 +0200

    updates.

--- Gizmo.Web.Api.Client/Clients/v3/SecurityProfilesWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/v3/SecurityProfilesWebApiClient.cs
@@ -1,7 +1,5 @@
 ﻿using Gizmo.Web.Api.Models;
-
 using Microsoft.Extensions.Options;
-
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,27 +9,39 @@ namespace Gizmo.Web.Api.Clients
     [WebApiRoute("api/v3/securityprofiles")]
     public sealed class SecurityProfilesWebApiClient : WebApiClientBase
     {
-        #region CONSTRUCTOR
         public SecurityProfilesWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
             base(httpClient, options, payloadSerializerProvider)
         {
         }
-        #endregion
 
-        #region FUNCTIONS
-
-        public Task<PagedList<SecurityProfileModel>> GetAsync(SecurityProfilesFilter filter, CancellationToken ct = default)
+        public Task<PagedList<SecurityProfileModel>> GetAsync(SecurityProfilesFilter filter, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(filter);
-            return GetAsync<PagedList<SecurityProfileModel>>(parameters, ct);
+            return GetAsync<PagedList<SecurityProfileModel>>(parameters, cancellationToken);
         }
 
-        public Task<SecurityProfileModel> GetByIdAsync(int id, CancellationToken ct = default)
+        public Task<SecurityProfileModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(id);
-            return GetAsync<SecurityProfileModel>(parameters, ct);
+            return GetAsync<SecurityProfileModel>(parameters, cancellationToken);
         }
 
-        #endregion
+        public Task<CreateResult> CreateAsync(SecurityProfileModelCreate model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateAsync(SecurityProfileModelUpdate model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, cancellationToken);
+        }
     }
 }
