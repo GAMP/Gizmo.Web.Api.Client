51e4360de07350640a9a8be003da1e56369d5001
Oleg Savelos <olegsavelos@gmail.com>
2025-02-07 17:08:22 +0200

    user guest client.

---
+++ Gizmo.Web.Api.Client/Clients/v3/UserGuestsWebApiClient.cs
@@ -0,0 +1,31 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/userguests")]
+    public sealed class UserGuestsWebApiClient : WebApiClientBase
+    {
+        public UserGuestsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<ReserveGuestResultModel> ReserveAsync(ReservedGuestModelCreate model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["reserve"]);
+            return PostAsync<ReserveGuestResultModel>(parameters,model, cancellationToken);
+        }
+
+        public Task<IEnumerable<ReservedGuestReleaseModel>> HostReleaseAsync(int hostId, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["host", hostId, "release"]);
+            return PostAsync<IEnumerable<ReservedGuestReleaseModel>>(parameters, null, cancellationToken);
+        }
+    }
+}
