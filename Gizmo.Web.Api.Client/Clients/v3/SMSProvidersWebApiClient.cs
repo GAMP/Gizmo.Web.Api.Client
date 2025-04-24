ff806065ae417430586435f621802f38fbf9fe57
klapsis-netprojects <klapsis@netprojects.gr>
2025-04-24 17:01:31 +0300

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/SMSProvidersWebApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/smsproviders")]
+    public sealed class SMSProvidersWebApiClient : WebApiClientBase
+    {
+        public SMSProvidersWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<IEnumerable<PaymentProviderMetadataModel>> GetAsync(CancellationToken ct = default)
+        {
+            var parameters = new UriParameters();
+            return GetAsync<IEnumerable<PaymentProviderMetadataModel>>(parameters, ct);
+        }
+    }
+}
