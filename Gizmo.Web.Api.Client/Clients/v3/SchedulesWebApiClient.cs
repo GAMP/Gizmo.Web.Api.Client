0115e5957490a350f3789ced9b64a0cecc075cab
Oleg Savelos <olegsavelos@gmail.com>
2025-05-16 10:23:03 +0300

    web client updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/SchedulesWebApiClient.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/schedules")]
+    public sealed class SchedulesWebApiClient : WebApiClientBase
+    {
+        public SchedulesWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public async Task<PagedList<ScheduleModel>> GetAsync(ScheduleFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return await GetAsync<PagedList<ScheduleModel>>(parameters, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<ScheduleModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return await GetAsync<ScheduleModel>(parameters, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<UpdateResult> EnableAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "enable"]);
+            return await PutAsync<UpdateResult>(parameters, null, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<UpdateResult> DisableAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "disable"]);
+            return await PutAsync<UpdateResult>(parameters, null, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<DeleteResult> DeleteAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return await DeleteAsync<DeleteResult>(parameters, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<UpdateResult> RenameAsync(int id, string name, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "name"]);
+            return await PutAsync<UpdateResult>(parameters, name, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<ExistResult> NameExistAsync(string name, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["name", name, "exist"]);
+            return await GetAsync<ExistResult>(parameters, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<UpdateResult> DescriptionAsync(int id, string description, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "description"]);
+            return await PutAsync<UpdateResult>(parameters, description, cancellationToken).ConfigureAwait(false);
+        }
+
+        public Task<CreateResult> CreateAsync(ScheduleReportCreateUpdateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, cancellationToken);
+        }
+
+        public Task<UpdateResult> UpdateAsync(int id, ScheduleReportCreateUpdateModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
+        }
+    }
+}
