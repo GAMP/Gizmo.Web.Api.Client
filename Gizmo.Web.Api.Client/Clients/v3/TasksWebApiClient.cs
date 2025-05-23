4d7bbdf721b7ded6e933298a15998903e6262faa
klapsis-netprojects <klapsis@netprojects.gr>
2025-05-23 16:40:41 +0300

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/TasksWebApiClient.cs
@@ -0,0 +1,48 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/tasks")]
+    public sealed class TasksWebApiClient : WebApiClientBase
+    {
+        #region CONSTRUCTOR
+        public TasksWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+        #endregion
+
+        #region FUNCTIONS
+
+        public Task<PagedList<TaskModel>> GetAsync(TaskFilter filter, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<TaskModel>>(parameters, ct);
+        }
+
+        public Task<TaskModel> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<TaskModel>(parameters, ct);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, ct);
+        }
+
+        public Task<PagedList<TaskCountersModel>> GetCountersAsync(TaskCountersFilter filter, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(["counters"], filter);
+            return GetAsync<PagedList<TaskCountersModel>>(parameters, ct);
+        }
+
+        #endregion
+    }
+}
