98dcd9ae460d5a49925e3530f8b294dd69bee5b8
Oleg Savelos <olegsavelos@gmail.com>
2025-10-21 16:05:57 +0300

    add payment intent web api client.

---
+++ Gizmo.Web.Api.Client/Clients/v3/PaymentIntentsWebApiClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Gizmo.Web.Api.Clients;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+
+namespace Gizmo.Web.Api.Client
+{
+    [WebApiRoute("api/v3/paymentintents")]
+    public sealed class PaymentIntentsWebApiClient : WebApiClientBase
+    {
+        public PaymentIntentsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<PaymentIntentModel>> GetAsync(PaymentIntentFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<PaymentIntentModel>>(parameters, cancellationToken);
+        }
+
+        public Task<PaymentIntentModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<PaymentIntentModel>(parameters, cancellationToken);
+        }
+
+        public Task<PaymentIntentModel> GetAsync(Guid identifier, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([identifier]);
+            return GetAsync<PaymentIntentModel>(parameters, cancellationToken);
+        }
+
+        public Task<PaymentIntentCreateResultModel> CreateAsync(PaymentIntentCreateParametersDepositModel model, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<PaymentIntentCreateResultModel>(parameters, model, cancellationToken);
+        }
+
+    }
+}
