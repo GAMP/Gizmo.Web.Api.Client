d6bb50e2c7969b00986f829bb2b5ffda61032bb1
Oleg Savelos <olegsavelos@gmail.com>
2026-01-29 13:21:25 +0200

    plural fixes.

--- Gizmo.Web.Api.Client/Clients/v3/OperatorsWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/v3/OperatorsWebApiClient.cs
@@ -1,7 +1,5 @@
 ﻿using Gizmo.Web.Api.Models;
-
 using Microsoft.Extensions.Options;
-
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,141 +14,153 @@ namespace Gizmo.Web.Api.Clients
         {
         }
 
-        public Task<PagedList<OperatorModel>> GetAsync(OperatorsFilter filter, CancellationToken ct = default)
+        public Task<PagedList<OperatorModel>> GetAsync(OperatorsFilter filter, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(filter);
-            return GetAsync<PagedList<OperatorModel>>(parameters, ct);
+            return GetAsync<PagedList<OperatorModel>>(parameters, cancellationToken);
         }
 
-        public Task<CreateResult> CreateAsync(OperatorModelCreate model, CancellationToken ct = default)
+        public Task<CreateResult> CreateAsync(OperatorModelCreate model, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters();
-            return PostAsync<CreateResult>(parameters, model, ct);
+            return PostAsync<CreateResult>(parameters, model, cancellationToken);
         }
 
-        public Task<UpdateResult> UpdateAsync(OperatorModelUpdate model, CancellationToken ct = default)
+        public Task<UpdateResult> UpdateAsync(OperatorModelUpdate model, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters();
-            return PutAsync<UpdateResult>(parameters, model, ct);
+            return PutAsync<UpdateResult>(parameters, model, cancellationToken);
         }
 
-        public Task<OperatorModel> GetByIdAsync(int id, CancellationToken ct = default)
+        public Task<OperatorModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(id);
-            return GetAsync<OperatorModel>(parameters, ct);
+            return GetAsync<OperatorModel>(parameters, cancellationToken);
         }
 
-        public Task<OperatorModel> CurrentAsync(CancellationToken ct = default)
+        public Task<OperatorModel> CurrentAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] {"current"});
-            return GetAsync<OperatorModel>(parameters, ct);
+            var parameters = new UriParameters(["current"]);
+            return GetAsync<OperatorModel>(parameters, cancellationToken);
         }
 
-        public Task<DeleteResult> DeleteAsync(int id, CancellationToken ct = default)
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters(id);
-            return DeleteAsync<DeleteResult>(parameters, ct);
+            return DeleteAsync<DeleteResult>(parameters, cancellationToken);
         }
 
         public Task<PagedList<BranchModel>> BranchesAsync(OpeatorBranchFilter filter, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "branches" }, filter);
+            var parameters = new UriParameters(["current", "branches"], filter);
             return GetAsync<PagedList<BranchModel>>(parameters, cancellationToken);
         }
 
-        public Task<CreateResult> AddToBranch(int operatorId, int branchId, CancellationToken cancellationToken = default)
+        public Task<CreateResult> AddToBranch(int id, int branchId, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { operatorId, "branch", branchId });
+            var parameters = new UriParameters([id, "branches", branchId]);
             return PostAsync<CreateResult>(parameters, cancellationToken);
         }
 
-        public Task<DeleteResult> RemoveFromBranchAsync(int operatorId, int branchId, CancellationToken cancellationToken = default)
+        public Task<DeleteResult> RemoveFromBranchAsync(int id, int branchId, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { operatorId, "branch", branchId });
+            var parameters = new UriParameters([id, "branches", branchId]);
             return DeleteAsync<DeleteResult>(parameters, cancellationToken);
+        }
+
+        public Task<UpdateResult> SetDefaultBranchAsync(int id, int branchId, bool isDefault, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "branches", branchId, "default", isDefault ]);
+            return PutAsync<UpdateResult>(parameters, cancellationToken);
+        }
+
+        public Task<int?> GetDefaultBranchAsync(int id,  CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "branches", "default"]);
+            return GetAsync<int?>(parameters, cancellationToken);
         }
 
         public Task<PagedList<BranchModel>> BranchesAsync(int operatorId, OpeatorBranchFilter filter, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { operatorId, "branches" }, filter);
+            var parameters = new UriParameters([operatorId, "branches"], filter);
             return GetAsync<PagedList<BranchModel>>(parameters, cancellationToken);
         }
 
         public Task<BranchModel?> BranchCurrentAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "branches", "current" });
+            var parameters = new UriParameters(["current", "branches", "current"]);
             return GetAsync<BranchModel?>(parameters, cancellationToken);
         }
 
         public Task<PagedList<RegisterModel>> RegistersAsync(int branchId, OperatorRegisterFilter filter, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "branches", branchId, "registers" }, filter);
+            var parameters = new UriParameters(["current", "branches", branchId, "registers"], filter);
             return GetAsync<PagedList<RegisterModel>>(parameters, cancellationToken);
         }
 
         public Task<PagedList<RegisterModel>> RegistersAsync(OperatorRegisterFilter filter, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "registers" }, filter);
+            var parameters = new UriParameters(["current", "registers"], filter);
             return GetAsync<PagedList<RegisterModel>>(parameters, cancellationToken);
         }
 
         public Task<RegisterModel?> RegisterCurrentAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "registers", "current" });
+            var parameters = new UriParameters(["current", "registers", "current"]);
             return GetAsync<RegisterModel?>(parameters, cancellationToken);
         }
 
         public Task<CreateResult> ShiftStartAsync(ShiftStartModel model, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new[] { "current", "shift", "start" });
+            var parameters = new UriParameters(["current", "shift", "start"]);
             return PostAsync<CreateResult>(parameters, model, cancellationToken);
         }
 
-        public Task<CreateResult> ShiftStartAsync(int operatorId, ShiftStartModel model, CancellationToken cancellationToken = default)
+        public Task<CreateResult> ShiftStartAsync(int id, ShiftStartModel model, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { operatorId, "shift", "start" });
+            var parameters = new UriParameters([id, "shift", "start"]);
             return PostAsync<CreateResult>(parameters, model, cancellationToken);
         }
 
         public Task<ActiveShiftModel> ShiftActiveAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "active" });
+            var parameters = new UriParameters(["current", "shift", "active"]);
             return GetAsync<ActiveShiftModel>(parameters, cancellationToken);
         }
 
         public Task<UpdateResult> ShiftActiveEndAsync(ShiftEndModel model, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "active", "end" });
+            var parameters = new UriParameters(["current", "shift", "active", "end"]);
             return PostAsync<UpdateResult>(parameters, model, cancellationToken);
         }
 
         public Task<UpdateResult> ShiftActiveLockAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "active", "lock" });
+            var parameters = new UriParameters(["current", "shift", "active", "lock"]);
             return PutAsync<UpdateResult>(parameters, null, cancellationToken);
         }
 
         public Task<UpdateResult> ShiftActiveUnlockAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "active", "unlock" });
+            var parameters = new UriParameters(["current", "shift", "active", "unlock"]);
             return PutAsync<UpdateResult>(parameters, null, cancellationToken);
         }
 
         public Task<ShiftOptionsModel> ShiftOptionsAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "options"});
+            var parameters = new UriParameters(["current", "shift", "options"]);
             return GetAsync<ShiftOptionsModel>(parameters, cancellationToken);
         }
 
         public Task<ShiftExpectedModel> ShiftActiveExpectedAsync(CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "shift", "active" , "expected" });
+            var parameters = new UriParameters(["current", "shift", "active" , "expected"]);
             return GetAsync<ShiftExpectedModel>(parameters, cancellationToken);
         }
 
         public Task<UpdateResult> PasswordUpdateAsync(OperatorPasswordUpdateModel model, CancellationToken cancellationToken = default)
         {
-            var parameters = new UriParameters(new object[] { "current", "password" });
+            var parameters = new UriParameters(["current", "password"]);
             return PostAsync<UpdateResult>(parameters, model, cancellationToken);
         }
 
@@ -164,6 +174,12 @@ namespace Gizmo.Web.Api.Clients
         {
             var parameters = new UriParameters([id, "disable"]);
             return PutAsync<UpdateResult>(parameters, null, cancellationToken);
+        }
+
+        public Task<UpdateResult> PermissionsSetAsync(int id, int permissionSetId, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters([id, "permissionsets", permissionSetId]);
+            return GetAsync<UpdateResult>(parameters, cancellationToken);
         }
 
     }
