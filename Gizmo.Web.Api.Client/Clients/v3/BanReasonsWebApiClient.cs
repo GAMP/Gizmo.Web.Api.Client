0c94cc5b68f630380d0a4d70418dc43d4e074642
klapsis-netprojects <klapsis@netprojects.gr>
2026-02-11 16:52:24 +0200

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/BanReasonsWebApiClient.cs
@@ -0,0 +1,56 @@
+﻿using Gizmo.Web.Api.Models;
+
+using Microsoft.Extensions.Options;
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/banreasons")]
+    public sealed class BanReasonsWebApiClient : WebApiClientBase
+    {
+        #region CONSTRUCTOR
+        public BanReasonsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+
+        }
+        #endregion
+
+        #region FUNCTIONS
+
+        public Task<PagedList<BanReasonModel>> GetAsync(BanReasonsFilter filter, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<BanReasonModel>>(parameters, ct);
+        }
+
+        public Task<CreateResult> CreateAsync(BanReasonModelCreate model, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters();
+            return PostAsync<CreateResult>(parameters, model, ct);
+        }
+
+        public Task<UpdateResult> UpdateAsync(BanReasonModelUpdate model, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters();
+            return PutAsync<UpdateResult>(parameters, model, ct);
+        }
+
+        public Task<BanReasonModel> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<BanReasonModel>(parameters, ct);
+        }
+
+        public Task<DeleteResult> DeleteAsync(int id, CancellationToken ct = default)
+        {
+            var parameters = new UriParameters(id);
+            return DeleteAsync<DeleteResult>(parameters, ct);
+        }
+
+        #endregion
+    }
+}
