f3e00972e265cb7ccfb075714ed1bae06830dc83
klapsis-netprojects <klapsis@netprojects.gr>
2026-02-12 12:55:25 +0200

    Updates.

---
+++ Gizmo.Web.Api.Client/Clients/v3/BanLogsWebApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using Gizmo.Web.Api.Models;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Gizmo.Web.Api.Clients
+{
+    [WebApiRoute("api/v3/banlogs")]
+    public sealed class BanLogsWebApiClient : WebApiClientBase
+    {
+        public BanLogsWebApiClient(HttpClient httpClient, IOptions<WebApiClientOptions> options, IPayloadSerializerProvider payloadSerializerProvider) :
+            base(httpClient, options, payloadSerializerProvider)
+        {
+        }
+
+        public Task<PagedList<BanLogModel>> GetAsync(BanLogsFilter filter, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(filter);
+            return GetAsync<PagedList<BanLogModel>>(parameters, cancellationToken);
+        }
+
+        public Task<BanLogModel> GetAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(id);
+            return GetAsync<BanLogModel>(parameters, cancellationToken);
+        }
+    }   
+}
