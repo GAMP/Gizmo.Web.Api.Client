9798eddef0ee60728a3e5d2313eb074dd1ddcae4
mrdpod <mr.d.pod@mail.ru>
2026-04-30 17:58:33 +0300

    add post method for exist username

--- Gizmo.Web.Api.Client/Clients/v3/UsersWebApiClient.cs
+++ Gizmo.Web.Api.Client/Clients/v3/UsersWebApiClient.cs
@@ -302,6 +302,12 @@ namespace Gizmo.Web.Api.Clients
             return GetAsync<ExistResult>(parameters, cancellationToken);
         }
 
+        public Task<ExistResult> UsernameExistAsync(string username, CancellationToken cancellationToken = default)
+        {
+            var parameters = new UriParameters(["username", username, "exist"]);
+            return GetAsync<ExistResult>(parameters, cancellationToken);
+        }
+
         public Task<UpdateResult> NegativeBalanceSetAsync(int id, UserNegativeBalanceSetModel model, CancellationToken cancellationToken = default)
         {
             var parameters = new UriParameters([id, "negativebalance"]);
